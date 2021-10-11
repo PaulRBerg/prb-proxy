16230f86f61cfe46edfdd6f3e8b1f3a62054e094
Paul Razvan Berg <hello@paulrberg.com>
2021-10-11 10:14:02 +0300

    chore(prb-proxy): delete "hardhat/console.sol" import

--- packages/prb-proxy/contracts/PRBProxy.sol
+++ packages/prb-proxy/contracts/PRBProxy.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity >=0.8.4;
 
-import "hardhat/console.sol";
 import "./IPRBProxy.sol";
 
 /// @notice Emitted when execution reverted with no reason.
