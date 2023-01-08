2a76c53ca4de1451c4ce4f1c92bb82e01c2eb96a
Paul Razvan Berg <hello@paulrberg.com>
2023-01-08 20:58:27 +0200

    test: move Solidity tests to "test" directory
    test: delete stale JavaScrip test for "execute" function

--- test-hardhat/contracts/index.test.ts
+++ test-hardhat/contracts/index.test.ts
@@ -1,8 +1,6 @@
 import { baseContext } from "../shared/contexts";
-import { integrationTestPrbProxy } from "./prbProxy/PRBProxy.test";
 import { integrationTestPrbProxyRegistry } from "./prbProxyRegistry/PRBProxyRegistry.test";
 
 baseContext("PRBProxy Solidity", function () {
-  integrationTestPrbProxy();
   integrationTestPrbProxyRegistry();
 });
