c5d32eb921b8fa804fd82f89c4ddfb137419bbd5
Paul Razvan Berg <hello@paulrberg.com>
2023-02-24 15:42:11 +0200

    feat: add "SetPermission" bootstrap script
    docs: change license in BaseScript

---
+++ script/bootstrap/SetPermission.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.18 <0.9.0;
+
+import { IPRBProxy } from "../../src/interfaces/IPRBProxy.sol";
+import { IPRBProxyHelpers } from "../../src/interfaces/IPRBProxyHelpers.sol";
+
+import { BaseScript } from "../shared/Base.s.sol";
+
+/// @notice Bootstraps the proxy system by giving permission to an envoy and installing a plugin.
+contract SetPermission is BaseScript {
+    function run(IPRBProxy proxy, IPRBProxyHelpers helpers, address target) public broadcaster {
+        // ABI encode the call to `setPermission`.
+        address envoy = vm.addr(vm.deriveKey(mnemonic, 1));
+        bool permission = true;
+        bytes memory data = abi.encodeCall(helpers.setPermission, (envoy, target, permission));
+
+        // Execute the call to the enshrined target.
+        proxy.execute(address(helpers), data);
+    }
+}
