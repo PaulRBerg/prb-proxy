a32b7b01ee0c7f22cfdab18469623cf70e8d9327
Paul Razvan Berg <hello@paulrberg.com>
2023-02-25 13:04:01 +0200

    refactor: pass response in "SetPermission"

--- script/bootstrap/SetPermission.s.sol
+++ script/bootstrap/SetPermission.s.sol
@@ -8,13 +8,21 @@ import { BaseScript } from "../shared/Base.s.sol";
 
 /// @notice Bootstraps the proxy system by giving permission to an envoy and installing a plugin.
 contract SetPermission is BaseScript {
-    function run(IPRBProxy proxy, IPRBProxyHelpers helpers, address target) public broadcaster {
+    function run(
+        IPRBProxy proxy,
+        IPRBProxyHelpers helpers,
+        address target
+    )
+        public
+        broadcaster
+        returns (bytes memory response)
+    {
         // ABI encode the call to `setPermission`.
         address envoy = vm.addr(vm.deriveKey(mnemonic, 1));
         bool permission = true;
         bytes memory data = abi.encodeCall(helpers.setPermission, (envoy, target, permission));
 
         // Execute the call to the enshrined target.
-        proxy.execute(address(helpers), data);
+        response = proxy.execute(address(helpers), data);
     }
 }
