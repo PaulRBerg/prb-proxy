b0ef3d8fdf99a53782c06322d95290dbb4c2cc42
Paul Razvan Berg <paul.razvan.berg@gmail.com>
2023-07-03 12:29:58 +0300

    refactor: rename deployer to broadcaster
    feat: use $ETH_FROM as broadcaster

--- script/SetPermission.s.sol
+++ script/SetPermission.s.sol
@@ -7,8 +7,8 @@ import { BaseScript } from "./Base.s.sol";
 
 /// @notice Permits an envoy to delegate call to a target contract.
 contract SetPermission is BaseScript {
-    function run(IPRBProxyRegistry registry, address target, bool permission) public broadcaster {
-        address envoy = vm.addr(vm.deriveKey(mnemonic, 1));
+    function run(IPRBProxyRegistry registry, address target, bool permission) public broadcast {
+        address envoy = vm.addr(vm.deriveKey({ mnemonic: mnemonic, index: 1 }));
         registry.setPermission({ envoy: envoy, target: target, permission: permission });
     }
 }
